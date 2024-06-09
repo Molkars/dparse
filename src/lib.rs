fae512846a748d891d89173620d9760f23824b00
Molkars <d.j.shaffer@outlook.com>
2024-06-09 12:38:19 -0600

    add [char; N] as valid pattern

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,11 @@ pub struct ParseError {
 
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "parse error occurred at {}:{} ({})", self.location.line, self.location.column, self.location.index)?;
+        writeln!(
+            f,
+            "parse error occurred at {}:{} ({})",
+            self.location.line, self.location.column, self.location.index
+        )?;
         writeln!(f, "  {}", self.message)?;
         Ok(())
     }
@@ -146,7 +150,6 @@ impl<'a> Parser<'a> {
         self.at_end() || filter.peek(self)
     }
 
-
     #[inline]
     pub fn take<P: ParsePrimitive>(&mut self, filter: P) -> bool {
         self.whitespace();
@@ -174,12 +177,19 @@ impl<'a> Parser<'a> {
     }
 
     #[inline]
-    pub fn expect<P: ParsePrimitive + Copy + Debug>(&mut self, filter: P) -> Result<(), ParseError> {
+    pub fn expect<P: ParsePrimitive + Copy + Debug>(
+        &mut self,
+        filter: P,
+    ) -> Result<(), ParseError> {
         self.whitespace();
         if filter.take(self) {
             Ok(())
         } else {
-            Err(ParseError::new_spanned(format!("Expected {:?}", filter), self.location, filter.len()))
+            Err(ParseError::new_spanned(
+                format!("Expected {:?}", filter),
+                self.location,
+                filter.len(),
+            ))
         }
     }
 
@@ -191,8 +201,7 @@ impl<'a> Parser<'a> {
 
     #[inline]
     pub fn take_char(&mut self) -> Option<char> {
-        self.peek_char()
-            .inspect(|c| self.location.advance(*c))
+        self.peek_char().inspect(|c| self.location.advance(*c))
     }
 
     #[inline]
@@ -221,6 +230,29 @@ impl ParsePrimitive for char {
         } else {
             false
         }
+    }
+
+    fn len(&self) -> usize {
+        1
+    }
+}
+
+impl<const N: usize> ParsePrimitive for [char; N] {
+    #[inline]
+    fn peek(self, parser: &Parser) -> bool {
+        parser.source[parser.location.index..]
+            .chars()
+            .next()
+            .is_some_and(|c| self.into_iter().any(|a| a == c))
+    }
+
+    fn take(self, parser: &mut Parser) -> bool {
+        parser.source[parser.location.index..]
+            .chars()
+            .next()
+            .and_then(|c| self.into_iter().find(|a| *a == c))
+            .inspect(|a| parser.location.advance(*a))
+            .is_some()
     }
 
     fn len(&self) -> usize {
@@ -283,16 +315,23 @@ impl<T: FnOnce(char) -> bool> ParsePrimitive for T {
 mod lock_test {
     use super::{Parser, ParserFilter};
 
-    struct WsLock<'a, 'parent> where 'a: 'parent {
+    struct WsLock<'a, 'parent>
+    where
+        'a: 'parent,
+    {
         previous: Option<ParserFilter>,
-        parser: &'parent mut MyParser<'a>
+        parser: &'parent mut MyParser<'a>,
     }
     impl<'a, 'b> std::ops::Deref for WsLock<'a, 'b> {
         type Target = MyParser<'a>;
-        fn deref(&self) -> &Self::Target { &self.parser }
+        fn deref(&self) -> &Self::Target {
+            &self.parser
+        }
     }
     impl<'a, 'b> std::ops::DerefMut for WsLock<'a, 'b> {
-        fn deref_mut(&mut self) -> &mut Self::Target { &mut self.parser }
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.parser
+        }
     }
     impl<'a, 'b> std::ops::Drop for WsLock<'a, 'b> {
         fn drop(&mut self) {
@@ -306,10 +345,14 @@ mod lock_test {
     }
     impl<'a> std::ops::Deref for MyParser<'a> {
         type Target = Parser<'a>;
-        fn deref(&self) -> &Self::Target { &self.inner }
+        fn deref(&self) -> &Self::Target {
+            &self.inner
+        }
     }
     impl std::ops::DerefMut for MyParser<'_> {
-        fn deref_mut(&mut self) -> &mut Self::Target { &mut self.inner }
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.inner
+        }
     }
 
     impl<'a> MyParser<'a> {
@@ -352,8 +395,8 @@ mod lock_test {
 #[test]
 fn whitespace_test() {
     const SRC: &str = " 1";
-    let mut parser = Parser::new(SRC)
-        .with_whitespace(|parser| while parser.take(char::is_whitespace) {});
+    let mut parser =
+        Parser::new(SRC).with_whitespace(|parser| while parser.take(char::is_whitespace) {});
     assert_eq!(Some('1'), parser.take_char());
 }
 
@@ -392,11 +435,12 @@ fn parser_demo() {
             }
             parser.expect('\'')?;
             Ok(content)
-        })).transpose()
+        }))
+        .transpose()
     };
 
-    let mut parser = Parser::new("-12 'hey!' okay?")
-        .with_whitespace(|p| while p.take(char::is_whitespace) {});
+    let mut parser =
+        Parser::new("-12 'hey!' okay?").with_whitespace(|p| while p.take(char::is_whitespace) {});
     assert!(matches!(parse_no(&mut parser), Ok(-12i32)));
     assert!(matches!(parse_str(&mut parser), Ok(Some(x)) if x == "hey!"));
     assert!(parser.expect("got it?").is_err());
